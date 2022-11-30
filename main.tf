f42b69fd4520bb4f97d9cf94d4db6d48fcc10c9f
rushikeshtayde <rushikeshtayde444@gmail.com>
2022-11-30 12:15:11 +0530

    Added terraform changes

---
+++ main.tf
@@ -0,0 +1,72 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "eu-west-1"
+}
+
+
+data "aws_iam_role" "task_ecs" {
+  name = "ecsTaskExecutionRole"
+}
+
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+data "aws_subnet_ids" "subnets" {
+  vpc_id = "${data.aws_vpc.default_vpc.id}"
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "flaskapp-ecsTaskRole"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_ecs_task_definition" "definition" {
+  family                   = "flaskapp"
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  execution_role_arn       = data.aws_iam_role.task_ecs.arn
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "1024"
+  requires_compatibilities = ["FARGATE"]
+
+  container_definitions = <<TASK_DEFINITION
+[
+  {
+    "image": " 303981612052.dkr.ecr.eu-west-2.amazonaws.com/docker-registry:latest",
+    "name": "flaskapp-container",
+    "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-region" : "eu-west-2",
+                    "awslogs-group" : "stream-to-log-fluentd",
+                    "awslogs-stream-prefix" : "flaskapp"
+                }
+            },
+
+    "environment": [
+            {
+                "name": "Flaskapp",
+                "value": "hello"
+            }
+        ]
+    }
+
+]
+TASK_DEFINITION
+}
